d856efaa332e4eec6f018e0da420edf8df145e3d
trongngk <53163510+trongngk@users.noreply.github.com>
2022-12-27 09:17:34 +0700

    Update signin.php

--- signin.php
+++ signin.php
@@ -4,8 +4,10 @@
     $username = $_POST['username'];
     $password =$_POST['password'];
 
-    $sql = "SELECT * FROM data WHERE username= ? and password= ?";
-    $sql->bind_param("ss",$username,$password);
+    $san_user = mysqli_real_escape_string($conn,$username);
+    $san_pass = mysqli_real_escape_string($conn,$password);
+
+    $sql = "SELECT * FROM data WHERE username = '$san_user' and password='$san_pass'";
     $result = mysqli_query($conn, $sql);
 
     if(mysqli_num_rows($result)>0) {
